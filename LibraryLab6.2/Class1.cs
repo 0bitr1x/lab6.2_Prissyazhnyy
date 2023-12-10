ab6f6dfd06703da773f90825e1051dfd9e6b7284
Victor <flyshoot954@gmail.com>
2023-12-10 05:15:05 +0200

    Добавьте файлы проекта.

---
+++ LibraryLab6.2/Class1.cs
@@ -0,0 +1,52 @@
+﻿namespace LibraryLab6._2
+{
+    public class Class1
+    {
+        public static double ArrayMas(int i, int j)
+        {
+            double calc = Math.Pow(Math.Log(7 * i + j + 3, 3), 3) - (-Math.Exp(i + j));
+            return calc;
+        }
+        public static double Vector(double[,] mas)
+        {
+            double resultVector = 0;
+            for (int i = 0; i < mas.GetLength(1); i++)
+            {
+                resultVector = 0;
+                for (int j = 0; j < mas.GetLength(0); j++) 
+                {
+                    resultVector += mas[j, i] > 0 ? Math.Pow(mas[j, i], 2): 0;
+                }
+                Console.Write($"вектор столбца {i + 1} = {resultVector:F2}");
+                Console.WriteLine();
+            }
+            return resultVector;
+        }
+        public static double Function(double[,] mas)
+        {
+            double max = 0, min = 0;
+            int maxI = 0, minI = 0;
+            for (int i = 0; i < mas.GetLength (1); i++)
+            {
+                double resultVector = 0;
+                for (int j = 0; j < mas.GetLength(0); j++)
+                {
+                    resultVector += mas[j, i] > 0 ? Math.Pow(mas[j, i], 2) : 0;
+                }
+                for (int j = 0; j < 1; j++) min = resultVector;
+                if (resultVector > max)
+                {
+                    max = resultVector;
+                    maxI = i;
+                }
+                if (resultVector < min)
+                {
+                    min = resultVector;
+                    minI = i;
+                }
+            }
+            Console.WriteLine($"функция = {maxI * minI}");
+            return maxI * minI;
+        }
+    }
+}
---
+++ lab6.2_Prissyazhnyy/Program.cs
@@ -0,0 +1,22 @@
+﻿using LibraryLab6._2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 4;
+        int m = 6;
+        double[,] mas = new double[n,m];
+        for(int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                mas[i, j] = Class1.ArrayMas(i, j);
+                Console.Write($"{mas[i, j]:F2}  ");
+            }
+            Console.WriteLine();
+        }
+        Class1.Vector( mas );
+        Class1.Function(mas);
+    }
+}
